eed44017f45f12f75a281fd646ef47368f867a9b
조경진 <kj_cho@idealidea.co.kr>
2020-07-14 21:38:17 +0900

    내 설정 프래그먼트 (빈화면으로) 추가

---
+++ app/src/main/java/kr/co/tjoeun/pizzastore_20200714/fragments/MyProfileFragment.kt
@@ -0,0 +1,20 @@
+package kr.co.tjoeun.pizzastore_20200714.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kr.co.tjoeun.pizzastore_20200714.R
+
+class MyProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_my_profile, container, false)
+    }
+
+}
