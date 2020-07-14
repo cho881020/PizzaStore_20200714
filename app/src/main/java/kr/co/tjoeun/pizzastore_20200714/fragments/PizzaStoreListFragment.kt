59ae5c0035c8d4c9113036d79679f1b5e967422b
조경진 <kj_cho@idealidea.co.kr>
2020-07-14 21:21:39 +0900

    프래그먼트에서 리스트뷰 / 어댑터 연결

--- app/src/main/java/kr/co/tjoeun/pizzastore_20200714/fragments/PizzaStoreListFragment.kt
+++ app/src/main/java/kr/co/tjoeun/pizzastore_20200714/fragments/PizzaStoreListFragment.kt
@@ -5,12 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_pizza_store_list.*
 import kr.co.tjoeun.pizzastore_20200714.R
+import kr.co.tjoeun.pizzastore_20200714.adapters.PizzaStoreAdapter
 import kr.co.tjoeun.pizzastore_20200714.datas.PizzaStore
 
 class PizzaStoreListFragment : Fragment() {
 
     val mPizzaStoreList = ArrayList<PizzaStore>()
+
+    lateinit var mAdapter : PizzaStoreAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,6 +31,10 @@ class PizzaStoreListFragment : Fragment() {
         mPizzaStoreList.add(PizzaStore("파파존스", "http://mblogthumb2.phinf.naver.net/20160530_65/ppanppane_1464617766007O9b5u_PNG/%C6%C4%C6%C4%C1%B8%BD%BA_%C7%C7%C0%DA_%B7%CE%B0%ED_%284%29.png?type=w800", "1577-8080"))
         mPizzaStoreList.add(PizzaStore("도미노 피자", "https://pbs.twimg.com/profile_images/1098371010548555776/trCrCTDN_400x400.png", "1577-3082"))
         mPizzaStoreList.add(PizzaStore("미스터 피자", "https://post-phinf.pstatic.net/MjAxODEyMDVfMzYg/MDAxNTQzOTYxOTA4NjM3.8gsStnhxz7eEc9zpt5nmSRZmI-Pzpl4NJvHYU-Dlgmcg.7Vpgk0lopJ5GoTav3CUDqmXi2-_67S5AXD0AGbbR6J4g.JPEG/IMG_1641.jpg?type=w1200", "1577-0077"))
+
+        mAdapter = PizzaStoreAdapter(activity!!, R.layout.pizza_store_list_item, mPizzaStoreList)
+
+        pizzaStoreListView.adapter = mAdapter
     }
 
 }
