40830e200aeaf05751f80774ec890523895efd57
조경진 <kj_cho@idealidea.co.kr>
2020-07-14 21:18:03 +0900

    피자가게 데이터를 목록에 추가

--- app/src/main/java/kr/co/tjoeun/pizzastore_20200714/fragments/PizzaStoreListFragment.kt
+++ app/src/main/java/kr/co/tjoeun/pizzastore_20200714/fragments/PizzaStoreListFragment.kt
@@ -6,8 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kr.co.tjoeun.pizzastore_20200714.R
+import kr.co.tjoeun.pizzastore_20200714.datas.PizzaStore
 
 class PizzaStoreListFragment : Fragment() {
+
+    val mPizzaStoreList = ArrayList<PizzaStore>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,6 +23,10 @@ class PizzaStoreListFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        mPizzaStoreList.add(PizzaStore("피자헛", "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FnkQca%2FbtqwVT4rrZo%2FymhFqW9uRbzrmZTxUU1QC1%2Fimg.jpg", "1588-5588"))
+        mPizzaStoreList.add(PizzaStore("파파존스", "http://mblogthumb2.phinf.naver.net/20160530_65/ppanppane_1464617766007O9b5u_PNG/%C6%C4%C6%C4%C1%B8%BD%BA_%C7%C7%C0%DA_%B7%CE%B0%ED_%284%29.png?type=w800", "1577-8080"))
+        mPizzaStoreList.add(PizzaStore("도미노 피자", "https://pbs.twimg.com/profile_images/1098371010548555776/trCrCTDN_400x400.png", "1577-3082"))
+        mPizzaStoreList.add(PizzaStore("미스터 피자", "https://post-phinf.pstatic.net/MjAxODEyMDVfMzYg/MDAxNTQzOTYxOTA4NjM3.8gsStnhxz7eEc9zpt5nmSRZmI-Pzpl4NJvHYU-Dlgmcg.7Vpgk0lopJ5GoTav3CUDqmXi2-_67S5AXD0AGbbR6J4g.JPEG/IMG_1641.jpg?type=w1200", "1577-0077"))
     }
 
 }
