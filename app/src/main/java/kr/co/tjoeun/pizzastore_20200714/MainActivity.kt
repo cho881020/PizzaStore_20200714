b6065b981246f92725b59184bb7122745b859fbd
조경진 <kj_cho@idealidea.co.kr>
2020-07-14 20:00:35 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/pizzastore_20200714/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.tjoeun.pizzastore_20200714
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
