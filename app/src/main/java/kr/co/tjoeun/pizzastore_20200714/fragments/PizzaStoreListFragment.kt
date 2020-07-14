435f6faf48cf9a4d58975b322457773511e92594
조경진 <kj_cho@idealidea.co.kr>
2020-07-14 20:50:39 +0900

    피자 가게 목록 표시 프래그먼트 생성

---
+++ app/src/main/java/kr/co/tjoeun/pizzastore_20200714/fragments/PizzaStoreListFragment.kt
@@ -0,0 +1,25 @@
+package kr.co.tjoeun.pizzastore_20200714.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kr.co.tjoeun.pizzastore_20200714.R
+
+class PizzaStoreListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_pizza_store_list, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+    }
+
+}
