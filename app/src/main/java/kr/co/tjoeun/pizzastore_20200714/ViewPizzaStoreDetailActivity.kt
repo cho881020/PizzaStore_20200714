543e42fda3e39f52378eccf400112d28a46e6f01
조경진 <kj_cho@idealidea.co.kr>
2020-07-14 22:20:19 +0900

    피자가게 상세화면 그림까지 추가

---
+++ app/src/main/java/kr/co/tjoeun/pizzastore_20200714/ViewPizzaStoreDetailActivity.kt
@@ -0,0 +1,24 @@
+package kr.co.tjoeun.pizzastore_20200714
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewPizzaStoreDetailActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_pizza_store_detail)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
+}
