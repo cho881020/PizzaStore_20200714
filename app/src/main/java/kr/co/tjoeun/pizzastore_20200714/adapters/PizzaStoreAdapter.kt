6bd5a60498961e5d6a6f94d7c86c5b631869df34
조경진 <kj_cho@idealidea.co.kr>
2020-07-14 22:07:42 +0900

    리스트뷰 실제 데이터 반영  - Glide라이브러리 추가 / 평문통신 허용

--- app/src/main/java/kr/co/tjoeun/pizzastore_20200714/adapters/PizzaStoreAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/pizzastore_20200714/adapters/PizzaStoreAdapter.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import kr.co.tjoeun.pizzastore_20200714.R
 import kr.co.tjoeun.pizzastore_20200714.datas.PizzaStore
 
@@ -24,6 +27,14 @@ class PizzaStoreAdapter(
 
         val row = tempRow!!
 
+        val logoImg = row.findViewById<ImageView>(R.id.logoImg)
+        val nameTxt = row.findViewById<TextView>(R.id.nameTxt)
+
+        val data = mList[position]
+
+        nameTxt.text = data.name
+
+        Glide.with(mContext).load(data.logoUrl).into(logoImg)
 
         return row
     }
