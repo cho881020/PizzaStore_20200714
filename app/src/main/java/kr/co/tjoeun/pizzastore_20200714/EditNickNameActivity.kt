4989d6e239ce76dcc55c992176b1e6516af68ad8
조경진 <kj_cho@idealidea.co.kr>
2020-07-15 20:58:50 +0900

    닉네임 변경 화면 추가

---
+++ app/src/main/java/kr/co/tjoeun/pizzastore_20200714/EditNickNameActivity.kt
@@ -0,0 +1,23 @@
+package kr.co.tjoeun.pizzastore_20200714
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNickNameActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nick_name)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
