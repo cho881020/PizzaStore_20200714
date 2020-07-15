b4c8c3b87dbaa2c422cf566605c9673938531ce0
조경진 <kj_cho@idealidea.co.kr>
2020-07-15 21:09:15 +0900

    닉네임 입력하고 돌아가기 까지 작업

--- app/src/main/java/kr/co/tjoeun/pizzastore_20200714/EditNickNameActivity.kt
+++ app/src/main/java/kr/co/tjoeun/pizzastore_20200714/EditNickNameActivity.kt
@@ -1,7 +1,10 @@
 package kr.co.tjoeun.pizzastore_20200714
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nick_name.*
 
 class EditNickNameActivity : BaseActivity() {
 
@@ -13,6 +16,22 @@ class EditNickNameActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+//        ok버튼을 누르면 완료 처리 => 데이터 들고 돌아가기
+
+        okBtn.setOnClickListener {
+
+//            입력한 닉네임을 받아야 들고 돌아감.
+            val inputNickName = newNickNameEdt.text.toString()
+
+//            결과로 닉네임 첨부 + OK눌렀다고 명시
+            val resultIntent = Intent()
+            resultIntent.putExtra("nickName", inputNickName)
+
+            setResult(Activity.RESULT_OK, resultIntent)
+            finish()
+
+        }
 
     }
 
