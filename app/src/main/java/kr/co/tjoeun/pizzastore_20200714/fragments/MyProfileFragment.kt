64b7ebe3539d2ebf5edca7b9461dd224ca0a12d8
조경진 <kj_cho@idealidea.co.kr>
2020-07-15 21:05:00 +0900

    프래그먼트에서 결과를 받으러 이동

--- app/src/main/java/kr/co/tjoeun/pizzastore_20200714/fragments/MyProfileFragment.kt
+++ app/src/main/java/kr/co/tjoeun/pizzastore_20200714/fragments/MyProfileFragment.kt
@@ -6,9 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_my_profile.*
+import kr.co.tjoeun.pizzastore_20200714.EditNickNameActivity
 import kr.co.tjoeun.pizzastore_20200714.R
 
 class MyProfileFragment : Fragment() {
+
+//    닉네임을 가지러 간다고 명시하는 숫자를 담은 변수
+    val REQ_FOR_NEW_NICKNAME = 2003
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,6 +21,22 @@ class MyProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_my_profile, container, false)
+    }
+
+//    프래그먼트의 동작 처리 담당 함수
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        changeNickNameBtn.setOnClickListener {
+
+            val myIntent = Intent(activity, EditNickNameActivity::class.java)
+
+//            결과를 가지러 이동할때의 화면 전환
+            startActivityForResult(myIntent, REQ_FOR_NEW_NICKNAME)
+
+        }
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
